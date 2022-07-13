c09450cfea064b8b89be24a4b332fc7fb04cfbed
grigory osipov <vn56@bk.ru>
2022-07-13 14:32:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/RecyclerAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.myapplication
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private var titles = arrayOf("Chapter One",
+        "Chapter Two",
+        "Chapter Three",
+        "Chapter Four",
+        "Chapter Five",
+        "Chapter Six",
+        "Chapter Seven",
+        "Chapter Eight")
+
+    private var details = arrayOf(
+        "Chapter One Details",
+        "Chapter Two Details", "Chapter Three Details",
+        "Chapter Four Details", "Chapter Five Details",
+        "Chapter Six Details",
+        "Chapter Seven Details",
+        "Chapter Eight Details")
+
+    private var images = arrayOf(
+        R.drawable.android,
+        R.drawable.android,
+        R.drawable.android,
+        R.drawable.android,
+        R.drawable.android,
+        R.drawable.android,
+        R.drawable.android,
+        R.drawable.android)
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetails: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetails = itemView.findViewById(R.id.item_detail)
+
+            itemView.setOnClickListener {
+                val position
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemTitle.text = titles[position]
+        holder.itemDetails.text = details[position]
+        holder.itemImage.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+}
